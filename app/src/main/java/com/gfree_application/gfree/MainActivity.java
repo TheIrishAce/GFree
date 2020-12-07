ef4af53eba9ba5cf579aa98e1fc4710cf8b1280f
Max Delaney <maxdelaney16@gmail.com>
2020-12-07 17:47:21 +0000

    Inital Project Creation | Added splash screen aswell as main activity

---
+++ app/src/main/java/com/gfree_application/gfree/MainActivity.java
@@ -0,0 +1,40 @@
+package com.gfree_application.gfree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                ImageView iv;
+                Button fade;
+                Animation animation;
+                iv = (ImageView)findViewById(R.id.imageView);
+                animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+                iv.startAnimation(animation);
+
+
+                Intent i = new Intent(MainActivity.this, Menu.class);
+                startActivity(i);
+                finish();
+            }
+        }, 10000);
+    }
+}

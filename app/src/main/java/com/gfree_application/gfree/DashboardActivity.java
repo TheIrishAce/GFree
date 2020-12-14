98b1351ae94672a89a8aaa383c1aab4b91976da5
Max Delaney <maxdelaney16@gmail.com>
2020-12-14 22:56:19 +0000

    first implmentation of barcode scanner

--- app/src/main/java/com/gfree_application/gfree/DashboardActivity.java
+++ app/src/main/java/com/gfree_application/gfree/DashboardActivity.java
@@ -1,25 +1,37 @@
 package com.gfree_application.gfree;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.widget.ToolbarWidgetWrapper;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     private Toolbar toolbar;
+
+    private Button barcodeScanner;
+    private TextView scannedData;
 
 
 
@@ -33,12 +45,37 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED); //Ask user for premission to use camera.
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-
+        scannedData = findViewById(R.id.resultsTextView);
     }
+
+    public void BarcodeScanner (View view){
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setBarcodeImageEnabled(false);
+        intentIntegrator.initiateScan();
+    }
+
+     @Override protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(intentResult != null){
+            if(intentResult.getContents() == null){
+                scannedData.setText("Incomplete Scan!");
+                Toast.makeText(this, "Incomplete Scan!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                scannedData.setText(intentResult.getContents());
+                Toast.makeText(this, intentResult.getContents().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+     }
+
+
     /*
        onNavigationItemSelected used to give functionality to items inside the hamburger menu.
        Add case for each item in the list.
@@ -71,3 +108,4 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
         }
     }
 }
+

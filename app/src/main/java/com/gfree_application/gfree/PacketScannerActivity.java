6c514710b85c03254651a9c523b619e93fbd8948
Max Delaney <maxdelaney16@gmail.com>
2020-12-17 15:36:16 +0000

    Rounded buttons

--- app/src/main/java/com/gfree_application/gfree/PacketScannerActivity.java
+++ app/src/main/java/com/gfree_application/gfree/PacketScannerActivity.java
@@ -15,6 +15,7 @@ import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +30,7 @@ public class PacketScannerActivity extends AppCompatActivity {
 
     private TextView textView;
     private SurfaceView surfaceView;
+    private Button captureScanButton;
 
     private CameraSource cameraSource;
     private TextRecognizer textRecognizer;
@@ -37,12 +39,15 @@ public class PacketScannerActivity extends AppCompatActivity {
 
     private String stringResult = null;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_packet_scanner);
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+        captureScanButton = findViewById(R.id.captureScanButton);
+        captureScanButton.setBackgroundResource(R.drawable.circular_button);
 
 //        if (String[]{Manifest.permission.CAMERA}) {
 //        }

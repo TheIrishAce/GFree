930f576a829b5f23da51a64689e6a25543f5f8e6
Max Delaney <maxdelaney16@gmail.com>
2021-04-25 01:45:32 +0100

    Completed Login and Register Prompts and Application Logo

--- app/src/main/java/com/gfree_application/gfree/DashboardPackage/HamburgerMenuPackage/LoginActivity.java
+++ app/src/main/java/com/gfree_application/gfree/DashboardPackage/HamburgerMenuPackage/LoginActivity.java
@@ -3,19 +3,25 @@ package com.gfree_application.gfree.DashboardPackage.HamburgerMenuPackage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.gfree_application.gfree.DashboardPackage.UserDashboardActivity;
 import com.gfree_application.gfree.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -53,12 +59,38 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser(String email, String pass) {
-        auth.signInWithEmailAndPassword(email, pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onSuccess(AuthResult authResult) {
-                Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(LoginActivity.this, UserDashboardActivity.class));
-                finish();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, UserDashboardActivity.class));
+                    finish();
+                } else {
+                    //if login fails
+                    try
+                    {
+                        throw task.getException();
+                    }
+                    // if user enters wrong email.
+                    catch (FirebaseAuthInvalidUserException invalidEmail)
+                    {
+                        Log.d("Login Check", "onComplete: invalid_email");
+                        Toast.makeText(LoginActivity.this, "Email is incorrect", Toast.LENGTH_SHORT).show();
+                        // TODO: take your actions!
+                    }
+                    // if user enters wrong password.
+                    catch (FirebaseAuthInvalidCredentialsException wrongPassword)
+                    {
+                        Log.d("Login Check", "onComplete: wrong_password");
+                        Toast.makeText(LoginActivity.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
+                        // TODO: Take your action
+                    }
+                    catch (Exception e)
+                    {
+                        Log.d("Login Check", "onComplete: " + e.getMessage());
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/gfree_application/gfree/DashboardPackage/HamburgerMenuPackage/RegisterActivity.java
+++ app/src/main/java/com/gfree_application/gfree/DashboardPackage/HamburgerMenuPackage/RegisterActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,11 +17,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
+    private EditText confirmPassword;
     private Button register;
 
     private FirebaseAuth auth;  //Global firebase auth variable.
@@ -32,6 +37,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         email = findViewById(R.id.RegisterEmailTextView);       //TextView
         password = findViewById(R.id.RegisterPasswordTextView); //Censored TextView
+        confirmPassword = findViewById(R.id.RegisterConfirmPasswordTextView); //Censored TextView
         register = findViewById(R.id.ConfirmRegisterButton);  //Button
 
         auth = FirebaseAuth.getInstance();
@@ -41,12 +47,16 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String emailText = email.getText().toString();
                 String passwordText = password.getText().toString();
+                String passwordConfirmText = confirmPassword.getText().toString();
 
                 if(TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText)){
-                    Toast.makeText(RegisterActivity.this, "Empty user credentials!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Empty user credentials!", Toast.LENGTH_LONG).show();
                 }
                 else if(passwordText.length() < 6){
-                    Toast.makeText(RegisterActivity.this, "Password too short, mimimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Password too short, mimimum 6 characters!", Toast.LENGTH_LONG).show();
+                }
+                else if(!passwordText.equals(passwordConfirmText) ){
+                    Toast.makeText(RegisterActivity.this, "Passwords don't match!", Toast.LENGTH_LONG).show();
                 }
                 else {
                     registerUser(emailText, passwordText);
@@ -63,6 +73,36 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this, "Registration Completed!", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                     finish();
+                }
+                else if (!task.isSuccessful()){
+                    try
+                    {
+                        throw task.getException();
+                    }
+                    // if user enters wrong email.
+                    catch (FirebaseAuthWeakPasswordException weakPassword)
+                    {
+                        Log.d("Registration Check", "onComplete: weak_password");
+                        Toast.makeText(RegisterActivity.this, "Weak Password!", Toast.LENGTH_SHORT).show();
+                        // TODO: take your actions!
+                    }
+                    // if user enters wrong password.
+                    catch (FirebaseAuthInvalidCredentialsException malformedEmail)
+                    {
+                        Log.d("Registration Check", "onComplete: malformed_email");
+                        Toast.makeText(RegisterActivity.this, "Email is malformed! Double check it.", Toast.LENGTH_SHORT).show();
+                        // TODO: Take your action
+                    }
+                    catch (FirebaseAuthUserCollisionException existEmail)
+                    {
+                        Log.d("Registration Check", "onComplete: exist_email");
+                        Toast.makeText(RegisterActivity.this, "Email is already linked to an account!", Toast.LENGTH_SHORT).show();
+                        // TODO: Take your action
+                    }
+                    catch (Exception e)
+                    {
+                        Log.d("Registration Check", "onComplete: " + e.getMessage());
+                    }
                 }
                 else {
                     Toast.makeText(RegisterActivity.this, "Registration Failed!", Toast.LENGTH_SHORT).show();
